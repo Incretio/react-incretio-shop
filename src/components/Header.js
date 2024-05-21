afcdecda290a1b163b025dc5a52415ad85780e9f
incretio <incretio@gmail.com>
2024-05-21 19:05:25 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <a href="/"
                    className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <span className="fs-4">Fortnite Shop</span>
+                    <span className="fs-4">Incretio Shop</span>
                 </a>
             </header>
         </div>
