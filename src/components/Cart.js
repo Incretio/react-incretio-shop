c3141fcb235167be2852b2b1320df29017a641d2
incretio <incretio@gmail.com>
2024-05-21 18:56:30 +0300

    update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,19 +7,19 @@ function Cart({order = [], removeFromCart, handleCartShow, changeQuantity, payCa
 
     return (
         <div className="cart">
-            <ul className="list-group">
+            <ul className="list-group overflow-auto" style={{maxHeight: '740px'}}>
                 <li className="list-group-item list-group-item-secondary fw-bold">
                     Cart
                     <GrClose className="card-icon-close" role="button" onClick={handleCartShow}/>
                 </li>
                 {order.length ?
                     <li className="list-group-item d-flex justify-content-between align-items-center">
-                        <div className="row" style={{width: '100%'}}>
-                            <span className="col-sm-5 col-xl-8 fw-bold">Title</span>
-                            <span className="col-sm-2 col-xl-1 fw-bold">Price</span>
-                            <span className="col-sm-2 col-xl-1 fw-bold">Quantity</span>
-                            <span className="col-sm-2 col-xl-1 fw-bold">Sum</span>
-                            <span className="col-sm-1 col-xl-1"></span>
+                        <div className="row" style={{width: '-webkit-fill-available'}}>
+                            <span className="col-12 col-sm-5 col-xl-8 fw-bold">Title</span>
+                            <span className="col-12 col-sm-2 col-xl-1 fw-bold">Price</span>
+                            <span className="col-12 col-sm-2 col-xl-1 fw-bold">Quantity</span>
+                            <span className="col-12 col-sm-2 col-xl-1 fw-bold">Sum</span>
+                            <span className="col-12 col-sm-1 col-xl-1"></span>
                         </div>
                     </li>
                     :
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,16 +4,16 @@ import { FaRegSquareMinus, FaRegSquarePlus } from "react-icons/fa6";
 function CartItem({id, title, price, quantity, removeFromCart, changeQuantity}) {
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
-            <div className="row" style={{width: '100%'}}>
-                <span className="col-sm-5 col-xl-8">{title}</span>
-                <span className="col-sm-2 col-xl-1">${price.toFixed(2)}</span>
-                <span className="col-sm-2 col-xl-1">
+            <div className="row" style={{width: '-webkit-fill-available'}}>
+                <span className="col-12 col-sm-5 col-xl-8">{title}</span>
+                <span className="col-12 col-sm-2 col-xl-1">${price.toFixed(2)}</span>
+                <span className="col-12 col-sm-2 col-xl-1">
                     <FaRegSquareMinus role="button" onClick={() => changeQuantity(id, quantity - 1)}/>
                     <span className="mx-1" style={{}}>{quantity}</span>
                     <FaRegSquarePlus role="button" onClick={() => changeQuantity(id, quantity + 1)}/>
                 </span>
-                <span className="col-sm-2 col-xl-1">${(price * quantity.toString()).toFixed(2)}</span>
-                <span className="col-sm-1 col-xl-1"><MdDelete role="button" onClick={() => removeFromCart(id)}/></span>
+                <span className="col-12 col-sm-2 col-xl-1">${(price * quantity.toString()).toFixed(2)}</span>
+                <span className="col-12 col-sm-1 col-xl-1"><MdDelete role="button" onClick={() => removeFromCart(id)}/></span>
             </div>
         </li>
     );
